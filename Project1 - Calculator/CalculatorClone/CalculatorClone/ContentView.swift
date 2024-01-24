24ee29f3cea836d90dde079f629eae3d5eba78de
Seungho Song <seungho3623@naver.com>
2024-01-25 02:28:47 +0900

    #Calculator - 너비, 높이 함수화

--- Project1 - Calculator/CalculatorClone/CalculatorClone/ContentView.swift
+++ Project1 - Calculator/CalculatorClone/CalculatorClone/ContentView.swift
@@ -7,16 +7,83 @@
 
 import SwiftUI
 
+enum ButtonType: String{
+    case one, two, three, four, five, six, seven, eight, nine, zero
+    case dot, equal, plus, minus, multiple, devide
+    case percent, oppsite, clear
+    
+    var buttonDisplayName: String{
+        switch self{
+        case .one :
+            return "1"
+        case .two:
+            return "2"
+        case .three:
+            return "3"
+        case .four:
+            return "4"
+        case .five:
+            return "5"
+        case .six:
+            return "6"
+        case .seven:
+            return "7"
+        case .eight:
+            return "8"
+        case .nine:
+            return "9"
+        case .zero:
+            return "0"
+        case .dot:
+            return "."
+        case .equal:
+            return "="
+        case .plus:
+            return "+"
+        case .minus:
+            return "-"
+        case .multiple:
+            return "X"
+        case .devide:
+            return "/"
+        case .percent:
+            return "%"
+        case .oppsite:
+            return "+/-"
+        case .clear:
+            return "C"
+        }
+    }
+    var backGroundColor: Color{
+        switch self{
+        case .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .zero, .dot:
+            return Color("NumberButton")
+        case .equal, .plus, .minus, .multiple, .devide:
+            return Color.orange
+        case .clear, .oppsite, .percent:
+            return Color.gray
+        }
+    }
+    var foreGroundColor: Color{
+        switch self{
+        case .clear, .oppsite, .percent:
+            return Color.black
+        default:
+            return Color.white
+        }
+    }
+}
+
 struct ContentView: View {
     
     @State private var totalNumber: String = "0"
     
-    private let buttonData: [[String]] = [
-        ["C", "+/-", "%", "/"],
-        ["7", "8", "9", "X"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "+"],
-        ["0", "0", ".", "="]
+    private let buttonData: [[ButtonType]] = [
+        [.clear, .oppsite, .percent, .devide],
+        [.seven, .eight, .nine, .multiple],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        [.zero, .dot, .equal]
     ]
     
     var body: some View {
@@ -34,16 +101,16 @@ struct ContentView: View {
                 
                 ForEach(buttonData, id: \.self){ line in
                     HStack{
-                        ForEach(line, id: \.self){ row in
+                        ForEach(line, id: \.self){ item in
                             Button{
-                                
+                                totalNumber = calcuatorNumber(button: item)
                             } label: {
-                                Text(row)
-                                    .frame(width: 80,
-                                           height: 80)
-                                    .background(Color("NumberButton"))
+                                Text(item.buttonDisplayName)
+                                    .frame(width: buttonWidth(button: item),
+                                           height: buttonHeight(button: item))
+                                    .background(item.backGroundColor)
                                     .cornerRadius(40)
-                                    .foregroundColor(.white)
+                                    .foregroundColor(item.foreGroundColor)
                                     .font(.system(size: 33))
                             }
                         }
@@ -52,6 +119,19 @@ struct ContentView: View {
             }
         }
     }
+}
+
+private func calcuatorNumber(button: ButtonType) -> String {
+    return "TEST"
+}
+
+private func buttonWidth(button: ButtonType) -> CGFloat {
+    return (button == .zero) ? (UIScreen.main.bounds.width - 5 * 10) / 4 * 2
+    : (UIScreen.main.bounds.width - 5 * 10) / 4
+}
+
+private func buttonHeight(button: ButtonType) -> CGFloat {
+    return (UIScreen.main.bounds.width - 5 * 10) / 4
 }
 
 struct ContentView_Previews: PreviewProvider {
