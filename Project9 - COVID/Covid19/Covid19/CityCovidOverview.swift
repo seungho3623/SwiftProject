bdee1f153aa79afa25eca4a5c3521b780d855730
Seungho Song <seungho3623@naver.com>
2024-01-29 01:36:07 +0900

    #Covid 19

---
+++ Project9 - COVID/Covid19/Covid19/CityCovidOverview.swift
@@ -0,0 +1,39 @@
+//
+//  CityCovidOverview.swift
+//  Covid19
+//
+//  Created by 송승호 on 2024/01/29.
+//
+
+import Foundation
+
+struct CityCovidOverview: Codable {
+  let korea: CovidOverview
+  let seoul: CovidOverview
+  let busan: CovidOverview
+  let daegu: CovidOverview
+  let incheon: CovidOverview
+  let gwangju: CovidOverview
+  let daejeon: CovidOverview
+  let ulsan: CovidOverview
+  let sejong: CovidOverview
+  let gyeonggi: CovidOverview
+  let chungbuk: CovidOverview
+  let chungnam: CovidOverview
+  let jeonbuk: CovidOverview
+  let jeonnam: CovidOverview
+  let gyeongbuk: CovidOverview
+  let gyeongnam: CovidOverview
+  let jeju: CovidOverview
+}
+
+struct CovidOverview: Codable {
+  let countryName: String
+  let newCase: String
+  let totalCase: String
+  let recovered: String
+  let death: String
+  let percentage: String
+  let newCcase: String
+  let newFcase: String
+}
